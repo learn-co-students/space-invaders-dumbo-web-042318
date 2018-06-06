b4482cb2904a2d5fae3b820395be2f8e74f1560c
teasacura <teasacuraster@gmail.com>
2018-06-06 16:15:48 -0400

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,25 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = "disengaged"
+    this.crew = crew
+    crew = crew.map(member => {
+      member.currentShip = this
+    })
+
+    // crew.map(function (member) {
+    //   member.currentShip = this
+    // }.bind(this))
+
+    if (crew.length === 0) {
+      this.docked = true
+    } else {
+      this.docked = false
+    }
+
+    this.phasersCharge = 'uncharged'
+  }
+}
