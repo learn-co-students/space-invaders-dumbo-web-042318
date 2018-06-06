a8660dbd4158363248255b9d16a5263a5b005680
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-06-06 17:01:09 -0400

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,34 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name
+    this.crew = crew
+    this.phasers = phasers
+    this.shields = shields
+    // this.constructor.all.push(this)
+    this.cloaked = false
+    this.warpDrive = "disengaged"
+    this.phasersCharge = "uncharged"
+    this.setDocked()
+    this.setPilot()
+  }
+
+  setDocked() {
+    if (this.crew.length > 0) {
+      this.docked = false
+    }else {
+      this.docked = true
+    }
+  }
+  // static allShips() {
+  //   console.log(this.all)
+  // }
+
+  setPilot() {
+    for(const pilot of this.crew) {
+      pilot.currentShip = this
+    }
+  }
+
+  
+}
+// Spaceship.all = []
