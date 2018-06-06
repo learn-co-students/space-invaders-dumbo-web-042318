0d551f4fac0dc0aeef9e0ee38a08e6a7d85ad5c4
jessebillard <jessebillard@mac.com>
2018-06-06 17:11:06 -0400

    builds space invaders

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,23 @@
-class Spaceship {}
+
+
+class Spaceship {
+    constructor(name, crew, phasers, shields, cloaked = false, warpDrive = "disengaged", docked = true, phasersCharge = "uncharged") {
+        this.name = name
+        this.phasers = phasers
+        this.shields = shields
+        this.cloaked = cloaked
+        this.warpDrive = warpDrive
+        this.phasersCharge = phasersCharge
+        this.crew = crew
+        this.docked = docked
+        
+        for(const member of this.crew){
+            member.currentShip = this   
+        }
+
+        if (this.crew.length !== 0){
+            this.docked = false
+        }
+    
+    }    
+}
