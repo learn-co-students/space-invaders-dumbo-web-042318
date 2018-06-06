589691c79acd5de7677bfa885ec022dd6e0ae9f0
travisdock <travisdock@gmail.com>
2018-06-06 16:24:08 -0400

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,38 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position) {
+    this.position = position
+
+    if (typeof(currentShip) === "undefined") {
+      this.currentShip = 'Looking for a Rig'
+    }
+  }
+
+    engageWarpDrive() {
+      if ((this.currentShip) === 'Looking for a Rig' || this.position !=='Pilot') {
+        return 'had no effect'
+      } else {
+        this.currentShip.engageWarpDrive = 'engaged'
+      }
+    }
+
+    setsInvisibility() {
+
+      if ((this.currentShip) === 'Looking for a Rig' || this.position !=='Defender') {
+        return 'had no effect'
+      } else {
+        this.currentShip.cloaked = true
+      }
+    }
+
+    chargePhasers() {
+      if ((this.currentShip) === 'Looking for a Rig' || this.position !=='Gunner') {
+        return 'had no effect'
+      } else {
+        this.currentShip.phasersCharge = 'charged'
+      }
+    }
+
+
+
+
+}
--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,23 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name
+    this.crew = crew
+
+    crew.forEach(function(member) {
+      member.currentShip = this
+    }.bind(this))
+
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = 'disengaged'
+    if (crew.length === 0){
+      this.docked = true
+    } else {
+      this.docked = false
+    }
+    this.phasersCharge = 'uncharged'
+  }
+
+
+}
